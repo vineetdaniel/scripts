3b8b546c6fdca5e3e58a24561993e5f8f38b1104
vineet daniel <vineetdaniel@gmail.com>
2015-12-29 13:21:19 +0530

    changed the load loop

--- load.rb
+++ load.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 request_uri = 'http://api-dev.politician.world/p'
 request_query = ''
 url = "#{request_uri}#{request_query}"
-for i in 1..100 do
+for i in 1..1000 do
 	buffer = open(url).read
 	puts i
 end
