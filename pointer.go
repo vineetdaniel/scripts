ea86ee86ea9990343361ead001697ed7769261d1
Vineet Daniel <vineetdaniel@gmail.com>
2016-10-07 00:07:32 +0530

    some modifications

--- pointer.go
+++ pointer.go
@@ -3,10 +3,18 @@ import "fmt"
 
 func zero(x int) {
 	x = 0
+	fmt.Println(x)
+}
+
+func pZero(xPtr *int) {
+	*xPtr = 2
 }
 
 func main() {
 	x := 5
+	y := 4
 	zero(x)
+	pZero(&y) 
 	fmt.Println(x)
+	fmt.Println(y)
 }
